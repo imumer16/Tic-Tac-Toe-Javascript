ecdbae385c703bae3ace276828bf2cb3319aa3b8
Umer Ahmed <84404257+imumer16@users.noreply.github.com>
2022-05-01 10:41:02 +0500

    Update javascript.js

--- include/javascript.js
+++ include/javascript.js
@@ -17,7 +17,7 @@ function myfunc() {
     if ((b1 == 'x' || b1 == 'X') && (b2 == 'x' ||
         b2 == 'X') && (b3 == 'x' || b3 == 'X')) {
         document.getElementById('print')
-            .innerHTML = " Player X won <br><br> <img src='https://i.gifer.com/1hzt.gif' alt='gif' width='300' height='300'>";
+            .innerHTML = " Player X won <br><br> <img src='https://www.icegif.com/wp-content/uploads/winner-icegif-7.gif' alt='gif' width='300' height='300'>";
         document.getElementById("b4").disabled = true;
         document.getElementById("b5").disabled = true;
         document.getElementById("b6").disabled = true;
@@ -29,7 +29,7 @@ function myfunc() {
     else if ((b1 == 'x' || b1 == 'X') && (b4 == 'x' ||
         b4 == 'X') && (b7 == 'x' || b7 == 'X')) {
         document.getElementById('print')
-            .innerHTML = " Player X won <br><br> <img src='https://i.gifer.com/1hzt.gif' alt='gif' width='300' height='300'>";
+            .innerHTML = " Player X won <br><br> <img src='https://www.icegif.com/wp-content/uploads/winner-icegif-7.gif' alt='gif' width='300' height='300'>";
         document.getElementById("b2").disabled = true;
         document.getElementById("b3").disabled = true;
         document.getElementById("b5").disabled = true;
@@ -42,7 +42,7 @@ function myfunc() {
     else if ((b7 == 'x' || b7 == 'X') && (b8 == 'x' ||
         b8 == 'X') && (b9 == 'x' || b9 == 'X')) {
         document.getElementById('print')
-            .innerHTML = " Player X won <br><br> <img src='https://i.gifer.com/1hzt.gif' alt='gif' width='300' height='300'>";
+            .innerHTML = " Player X won <br><br> <img src='https://www.icegif.com/wp-content/uploads/winner-icegif-7.gif' alt='gif' width='300' height='300'>";
         document.getElementById("b1").disabled = true;
         document.getElementById("b2").disabled = true;
         document.getElementById("b3").disabled = true;
@@ -54,7 +54,7 @@ function myfunc() {
     else if ((b3 == 'x' || b3 == 'X') && (b6 == 'x' ||
         b6 == 'X') && (b9 == 'x' || b9 == 'X')) {
         document.getElementById('print')
-            .innerHTML = " Player X won <br><br> <img src='https://i.gifer.com/1hzt.gif' alt='gif' width='300' height='300'>";
+            .innerHTML = " Player X won <br><br> <img src='https://www.icegif.com/wp-content/uploads/winner-icegif-7.gif' alt='gif' width='300' height='300'>";
         document.getElementById("b1").disabled = true;
         document.getElementById("b2").disabled = true;
         document.getElementById("b4").disabled = true;
@@ -66,7 +66,7 @@ function myfunc() {
     else if ((b1 == 'x' || b1 == 'X') && (b5 == 'x' ||
         b5 == 'X') && (b9 == 'x' || b9 == 'X')) {
         document.getElementById('print')
-            .innerHTML = " Player X won <br><br> <img src='https://i.gifer.com/1hzt.gif' alt='gif' width='300' height='300'>";
+            .innerHTML = " Player X won <br><br> <img src='https://www.icegif.com/wp-content/uploads/winner-icegif-7.gif' alt='gif' width='300' height='300'>";
         document.getElementById("b2").disabled = true;
         document.getElementById("b3").disabled = true;
         document.getElementById("b4").disabled = true;
@@ -78,7 +78,7 @@ function myfunc() {
     else if ((b3 == 'x' || b3 == 'X') && (b5 == 'x' ||
         b5 == 'X') && (b7 == 'x' || b7 == 'X')) {
         document.getElementById('print')
-          .innerHTML = " Player X won <br><br> <img src='https://i.gifer.com/1hzt.gif' alt='gif' width='300' height='300'>";
+          .innerHTML = " Player X won <br><br> <img src='https://www.icegif.com/wp-content/uploads/winner-icegif-7.gif' alt='gif' width='300' height='300'>";
         document.getElementById("b1").disabled = true;
         document.getElementById("b2").disabled = true;
         document.getElementById("b4").disabled = true;
@@ -90,7 +90,7 @@ function myfunc() {
     else if ((b2 == 'x' || b2 == 'X') && (b5 == 'x' ||
         b5 == 'X') && (b8 == 'x' || b8 == 'X')) {
         document.getElementById('print')
-          .innerHTML = " Player X won <br><br> <img src='https://i.gifer.com/1hzt.gif' alt='gif' width='300' height='300'>";
+          .innerHTML = " Player X won <br><br> <img src='https://www.icegif.com/wp-content/uploads/winner-icegif-7.gif' alt='gif' width='300' height='300'>";
         document.getElementById("b1").disabled = true;
         document.getElementById("b3").disabled = true;
         document.getElementById("b4").disabled = true;
@@ -102,7 +102,7 @@ function myfunc() {
     else if ((b4 == 'x' || b4 == 'X') && (b5 == 'x' ||
         b5 == 'X') && (b6 == 'x' || b6 == 'X')) {
         document.getElementById('print')
-           .innerHTML = " Player X won <br><br> <img src='https://i.gifer.com/1hzt.gif' alt='gif' width='300' height='300'>";
+           .innerHTML = " Player X won <br><br> <img src='https://www.icegif.com/wp-content/uploads/winner-icegif-7.gif' alt='gif' width='300' height='300'>";
         document.getElementById("b1").disabled = true;
         document.getElementById("b2").disabled = true;
         document.getElementById("b3").disabled = true;
@@ -118,7 +118,7 @@ function myfunc() {
     else if ((b1 == '0' || b1 == '0') && (b2 == '0' ||
         b2 == '0') && (b3 == '0' || b3 == '0')) {
         document.getElementById('print')
-            .innerHTML = " Player 0 won <br><br> <img src='https://i.gifer.com/1hzt.gif' alt='gif' width='300' height='300'>";
+            .innerHTML = " Player 0 won <br><br> <img src='https://www.icegif.com/wp-content/uploads/winner-icegif-7.gif' alt='gif' width='300' height='300'>";
         document.getElementById("b4").disabled = true;
         document.getElementById("b5").disabled = true;
         document.getElementById("b6").disabled = true;
@@ -130,7 +130,7 @@ function myfunc() {
     else if ((b1 == '0' || b1 == '0') && (b4 == '0' ||
         b4 == '0') && (b7 == '0' || b7 == '0')) {
         document.getElementById('print')
-            .innerHTML = "Player 0 won <br><br> <img src='https://i.gifer.com/1hzt.gif' alt='gif' width='300' height='300'>";
+            .innerHTML = "Player 0 won <br><br> <img src='https://www.icegif.com/wp-content/uploads/winner-icegif-7.gif' alt='gif' width='300' height='300'>";
         document.getElementById("b2").disabled = true;
         document.getElementById("b3").disabled = true;
         document.getElementById("b5").disabled = true;
@@ -142,7 +142,7 @@ function myfunc() {
     else if ((b7 == '0' || b7 == '0') && (b8 == '0' ||
         b8 == '0') && (b9 == '0' || b9 == '0')) {
         document.getElementById('print')
-            .innerHTML = "Player 0 won <br><br> <img src='https://i.gifer.com/1hzt.gif' alt='gif' width='300' height='300'>";
+            .innerHTML = "Player 0 won <br><br> <img src='https://www.icegif.com/wp-content/uploads/winner-icegif-7.gif' alt='gif' width='300' height='300'>";
         document.getElementById("b1").disabled = true;
         document.getElementById("b2").disabled = true;
         document.getElementById("b3").disabled = true;
@@ -154,7 +154,7 @@ function myfunc() {
     else if ((b3 == '0' || b3 == '0') && (b6 == '0' ||
         b6 == '0') && (b9 == '0' || b9 == '0')) {
         document.getElementById('print')
-            .innerHTML = "Player 0 won <br><br><img src='https://i.gifer.com/1hzt.gif' alt='gif' width='300' height='300'>";
+            .innerHTML = "Player 0 won <br><br><img src='https://www.icegif.com/wp-content/uploads/winner-icegif-7.gif' alt='gif' width='300' height='300'>";
         document.getElementById("b1").disabled = true;
         document.getElementById("b2").disabled = true;
         document.getElementById("b4").disabled = true;
@@ -166,7 +166,7 @@ function myfunc() {
     else if ((b1 == '0' || b1 == '0') && (b5 == '0' ||
         b5 == '0') && (b9 == '0' || b9 == '0')) {
         document.getElementById('print')
-            .innerHTML = "Player 0 won <br><br> <img src='https://i.gifer.com/1hzt.gif' alt='gif' width='300' height='300'>";
+            .innerHTML = "Player 0 won <br><br> <img src='https://www.icegif.com/wp-content/uploads/winner-icegif-7.gif' alt='gif' width='300' height='300'>";
         document.getElementById("b2").disabled = true;
         document.getElementById("b3").disabled = true;
         document.getElementById("b4").disabled = true;
@@ -178,7 +178,7 @@ function myfunc() {
     else if ((b3 == '0' || b3 == '0') && (b5 == '0' ||
         b5 == '0') && (b7 == '0' || b7 == '0')) {
         document.getElementById('print')
-            .innerHTML = "Player 0 won  <br><br> <img src='https://i.gifer.com/1hzt.gif' alt='gif' width='300' height='300'>";
+            .innerHTML = "Player 0 won  <br><br> <img src='https://www.icegif.com/wp-content/uploads/winner-icegif-7.gif' alt='gif' width='300' height='300'>";
         document.getElementById("b1").disabled = true;
         document.getElementById("b2").disabled = true;
         document.getElementById("b4").disabled = true;
@@ -190,7 +190,7 @@ function myfunc() {
     else if ((b2 == '0' || b2 == '0') && (b5 == '0' ||
         b5 == '0') && (b8 == '0' || b8 == '0')) {
         document.getElementById('print')
-            .innerHTML = "Player 0 won <br><br> <img src='https://i.gifer.com/1hzt.gif' alt='gif' width='300' height='300'>";
+            .innerHTML = "Player 0 won <br><br> <img src='https://www.icegif.com/wp-content/uploads/winner-icegif-7.gif' alt='gif' width='300' height='300'>";
         document.getElementById("b1").disabled = true;
         document.getElementById("b3").disabled = true;
         document.getElementById("b4").disabled = true;
@@ -202,7 +202,7 @@ function myfunc() {
     else if ((b4 == '0' || b4 == '0') && (b5 == '0' ||
         b5 == '0') && (b6 == '0' || b6 == '0')) {
         document.getElementById('print')
-            .innerHTML = "Player 0 won <br><br> <img src='https://i.gifer.com/1hzt.gif' alt='gif' width='300' height='300'>";
+            .innerHTML = "Player 0 won <br><br> <img src='https://www.icegif.com/wp-content/uploads/winner-icegif-7.gif' alt='gif' width='300' height='300'>";
         document.getElementById("b1").disabled = true;
         document.getElementById("b2").disabled = true;
         document.getElementById("b3").disabled = true;
